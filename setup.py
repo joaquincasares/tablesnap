eadcbd3df62debae54c29f571519a76bfa07a589
Joaquin Casares <joaquin@datastax.com>
2012-02-17 16:58:29 -0600

    Now allow for configuration presets

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='Jeremy Grosser',
     author_email='jeremy@synack.me',
     scripts=['tablesnap'],
+    data_files=[('/etc/tablesnap', ['tablesnap.conf'])],
     install_requires=[
         'pyinotify',
         'boto',
